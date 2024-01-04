2d3d1b22f367482d047a2c3f077c328f2c482827
Italo Penaforte <italopenaforte@gmail.com>
2024-01-03 21:21:04 -0300

    config: setup base application

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='databasedb',
+    version='0.1',
+    description='An database for databases',
+    author='Italo',
+    author_email='italopenaforte@gmail.com',
+    packages=['databasedb']
+)
