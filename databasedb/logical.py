c3e6a6a593b08c2195a5339712f84d3d08c9fea5
Italo Penaforte <italopenaforte@gmail.com>
2024-01-04 22:18:27 -0300

    feat: add typing hint logical struct

--- databasedb/logical.py
+++ databasedb/logical.py
@@ -45,18 +45,18 @@ class LogicalBase(object):
         self._tree_ref = self.node_ref_class(
             address=self._storage.get_root_address())
 
-    def get(self, key):
+    def get(self, key: str):
         if not self._storage.locked:
             self._refresh_tree_ref()
         return self._get(self._follow(self._tree_ref), key)
 
-    def set(self, key, value):
+    def set(self, key: str, value: str | int):
         if self._storage.lock():
             self._refresh_tree_ref()
         self._tree_ref = self._insert(
             self._follow(self._tree_ref), key, self.value_ref_class(value))
 
-    def pop(self, key):
+    def pop(self, key: str):
         if self._storage.lock():
             self._refresh_tree_ref()
         self._tree_ref = self._delete(
